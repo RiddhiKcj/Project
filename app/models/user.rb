2a30e79c63ea7aafe380f9d0a7ba4b67646655dc
RiddhiKcj <ridds509@gmail.com>
2019-10-21 17:29:37 +0530

    email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   validates_presence_of :username
   validates_uniqueness_of :username, :email, :on => :create
   validates_presence_of :email, :on => :create
+  validates_format_of  :email, :with => /^[\+A-Z0-9\._%-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i, :multiline => true
+  validates :password, :presence => true, :confirmation => true, :length => {:within => 6..40}, :on => :create
+  validates :password, :length => {:within => 6..40}, :allow_blank => true, :on => :update
 end
 
 
